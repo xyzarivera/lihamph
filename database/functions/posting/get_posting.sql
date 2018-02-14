0fd62f5c6f42ffd3ba728c12066aad6c13435d6f
pasoc30 <kenneth.g.bastian@descouvre.com>
2018-02-15 00:00:43 +0800

    added tags on posting page

--- database/functions/posting/get_posting.sql
+++ database/functions/posting/get_posting.sql
@@ -8,6 +8,7 @@ CREATE OR REPLACE FUNCTION posting.get_posting (
   author_username VARCHAR(50),
   title VARCHAR(500),
   content TEXT,
+  tags VARCHAR[],
   is_edited BOOLEAN,
   is_deleted BOOLEAN,
   created_date TIMESTAMPTZ,
@@ -24,6 +25,7 @@ BEGIN
     pr.username,
     p.title,
     p.content,
+    tg.tags,
     p.is_edited,
     p.is_deleted,
     p.created_date,
@@ -31,6 +33,16 @@ BEGIN
   FROM posting.post AS p
   INNER JOIN core.person AS pr
     ON p.author_id = pr.person_id
+  LEFT JOIN (
+    SELECT
+      array_agg(tg.tag_name) AS tags,
+      ttg.topic_id
+    FROM posting.tag AS tg
+    INNER JOIN posting.topic_tag AS ttg
+      ON ttg.tag_id = tg.tag_id
+    GROUP BY ttg.topic_id
+  ) AS tg
+    ON tg.topic_id = p.topic_id
   WHERE p.topic_id = p_topic_id
     AND p.is_deleted = false
   ORDER BY p.parent_post_id NULLS FIRST, p.post_id;
